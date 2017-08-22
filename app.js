43d50c8643c7a7d3cd277091a5949a46c0b95af5
Norbert Szulc <norbert@not7cd.net>
2017-08-22 19:36:22 +0200

    Shameless copy pasting

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({
+    message: 'hooray! welcome to our api!'
+  });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
 
 app.listen(3000, function() {
   console.log('listening on port 3000')
