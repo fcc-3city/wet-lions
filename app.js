fb2795211251bfaa864e9f57fd45395fa0e48ade
Aleksander Wielgorski <olo_25_52@wp.pl>
2017-08-22 20:12:56 +0200

    adding hardcoded JSON for /api/measurements

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
+const harcodeJSON = `[
+  {
+    "date": "2016-05-11 05:00:00",
+    "station_id": 0,
+    "temp": 20,
+    "water": 2,
+    "rain": 0.2,
+    "wind": {
+      "dir": "NE",
+      "level": 1
+    }
+  },
+  {
+    "date": "2016-05-11 06:00:00",
+    "station_id": 0,
+    "temp": 20,
+    "water": 2,
+    "rain": 0.2,
+    "wind": {
+      "dir": "NE",
+      "level": 1
+    }
+  },
+  {
+    "date": "2016-05-11 07:00:00",
+    "station_id": 0,
+    "temp": 20,
+    "water": 2,
+    "rain": 0.2,
+    "wind": {
+      "dir": "NE",
+      "level": 1
+    }
+  }
+]`
+
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-app.listen(3000, function() {
-  console.log('listening on port 3000')
+app.get('/api/measurements', (req, res) => {
+    res.status(200)
+    res.set('Content-Type', 'application/json')
+    res.send(JSON.parse(harcodeJSON))
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
 })
