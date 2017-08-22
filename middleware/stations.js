406127b783b46320d2b79f4126f37428550cfedc
Norbert Szulc <norbert@not7cd.net>
2017-08-22 20:35:07 +0200

    Move to fetch

--- middleware/stations.js
+++ middleware/stations.js
@@ -1,29 +1,33 @@
-const request = require("request");
-// const https = require("https");
+const fetch = require("node-fetch");
 
-function unenvelope(envelope, callback) {
-  data = envelope['data']
-  callback(data)
-  // console.log(data);
-  // callback(data)
-  // return data
+function unenvelope(envelope) {
+  return envelope['data']
 }
 
 function stations(callback) {
-  request('http://pomiary.gdmel.pl/rest/stations', (error, response, body) => {
-    if(!error && response.statusCode == 200) {
-      console.log(body) // Print the google web page.
-      body = JSON.parse(body)
-      unenvelope(body, (data) => {
-        data.sort(function(a, b) {
-          return parseFloat(a.no) - parseFloat(b.no);
-        });
-        console.log(data);
-        callback(data);
-      });
 
-    }
-  })
+  fetch('http://pomiary.gdmel.pl/rest/stations')
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      return json;
+    }).then((json) => {
+      const data = unenvelope(json);
+      console.log(data);
+    });
+
+  // request('http://pomiary.gdmel.pl/rest/stations', (error, response, body) => {
+  //   if(!error && response.statusCode == 200) {
+  //     console.log(body) // Print the google web page.
+  //     body = JSON.parse(body)
+  //     data.sort(function(a, b) {
+  //       return parseFloat(a.no) - parseFloat(b.no);
+  //     });
+  //     console.log(data);
+  //     callback(data);
+  //
+  //   }
+  // })
 }
 
 stations(() => {})
