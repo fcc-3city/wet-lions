938af5aef3acc8ecad575725f8e66700d84e3ba5
Norbert Szulc <norbert@not7cd.net>
2017-08-29 19:07:37 +0200

    Map fetched data to new schema

--- middleware/stations.js
+++ middleware/stations.js
@@ -4,30 +4,42 @@ function unenvelope(envelope) {
   return envelope['data']
 }
 
-function stations(callback) {
+function fetchStations(callback) {
 
   fetch('http://pomiary.gdmel.pl/rest/stations')
     .then((res) => res.json())
     .then((json) => {
       console.log(json);
+
       return json;
     }).then((json) => {
       const data = unenvelope(json);
-      console.log(data);
+      return data
+    }).then((result) => {
+      console.log(result);
+      for(item of result) {
+        console.log(item);
+        const blargh = callback(item)
+        console.log(blargh);
+      }
     });
-
-  // request('http://pomiary.gdmel.pl/rest/stations', (error, response, body) => {
-  //   if(!error && response.statusCode == 200) {
-  //     console.log(body) // Print the google web page.
-  //     body = JSON.parse(body)
-  //     data.sort(function(a, b) {
-  //       return parseFloat(a.no) - parseFloat(b.no);
-  //     });
-  //     console.log(data);
-  //     callback(data);
-  //
-  //   }
-  // })
 }
 
-stations(() => {})
+function stationsToModel(data) {
+  const result = {
+    id: data.no - 1,
+    externalId: data.no,
+    name: data.name,
+    active: data.active,
+    sensors: {
+      rain: data.rain,
+      water: data.water,
+      windDir: data.winddir,
+      windLevel: data.windlevel
+    }
+  }
+  return result
+}
+
+const data = fetchStations(stationsToModel);
+console.log(data);
