4bc99dec30394128fe1b9cc1e642282eec6fb78f
Norbert Szulc <norbert@not7cd.net>
2017-08-30 10:12:17 +0200

    Clean station fetch

--- middleware/stations.js
+++ middleware/stations.js
@@ -4,24 +4,18 @@ function unenvelope(envelope) {
   return envelope['data']
 }
 
-function fetchStations(callback) {
-
-  fetch('http://pomiary.gdmel.pl/rest/stations')
+function fetchStations() {
+  return fetch('http://pomiary.gdmel.pl/rest/stations')
     .then((res) => res.json())
-    .then((json) => {
-      // console.log(json);
+    .then((json) => unenvelope(json))
+    .then((result) => {
+      let list = []
 
-      return json;
-    }).then((json) => {
-      const data = unenvelope(json);
-      return data
-    }).then((result) => {
-      // console.log(result);
       for(item of result) {
-        // console.log(item);
-        const blargh = callback(item)
-        // console.log(blargh);
+        list.push(stationsToModel(item))
       }
+
+      return list
     });
 }
 
@@ -41,5 +35,7 @@ function stationsToModel(data) {
   return result
 }
 
-// const data = fetchStations(stationsToModel);
-// console.log(data);
+
+const data = fetchStations().then((list) => {
+  console.log(list);
+})
