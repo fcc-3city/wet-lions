387744a008ba7175ea959e998aeabc23b3d92384
Norbert Szulc <norbert@not7cd.net>
2017-09-18 20:50:04 +0200

    Merge measurments in one object

--- middleware/measurments.js
+++ middleware/measurments.js
@@ -1,21 +1,27 @@
 const fetch = require("node-fetch");
 const moment = require('moment');
+const merge = require('merge')
 
 function fetchMeasurments(stationId, date) {
   return Promise.all([
     fetchMeasurmentsFromSensor(stationId, "rain", date),
     fetchMeasurmentsFromSensor(stationId, "water", date),
-    fetchMeasurmentsFromSensor(stationId, "winddir", date),
-    fetchMeasurmentsFromSensor(stationId, "windlevel", date),
+    fetchMeasurmentsFromSensor(stationId, "windDir", date),
+    fetchMeasurmentsFromSensor(stationId, "windLevel", date),
   ])
-  .then(data => data)
+  .then(list => merge(...list))
   .then(data => console.log(data));
 }
 
 function fetchMeasurmentsFromSensor(stationId, sensor, date) {
-  return fetch(`http://pomiary.gdmel.pl/rest/measurments/${stationId + 1}/${sensor}/${date}`)
+  return fetch(`http://pomiary.gdmel.pl/rest/measurments/${stationId + 1}/${sensor.toLowerCase()}/${date}`)
     .then((res) => res.json())
-    .then((json) => json['data'])
+    .then((json) => {
+      data = {};
+      data[sensor] = json['data'];
+      return data;
+    })
+    // .then(data => console.log(data));
 }
 
-console.log(fetchMeasurments(0, moment().format("YYYY-MM-DD")))
+fetchMeasurments(0, moment().format("YYYY-MM-DD"))
