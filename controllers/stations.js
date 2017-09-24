fb62ce72e2a5fb4565c3a9a76c30770ef02433b7
Norbert Szulc <norbert@not7cd.net>
2017-09-24 16:28:14 +0200

    Sort stations

--- controllers/stations.js
+++ controllers/stations.js
@@ -4,7 +4,8 @@ function fetchStations() {
   return fetch('http://pomiary.gdmel.pl/rest/stations')
     .then(res => res.json())
     .then(json => json.data)
-    .then(result => result.map(station => stationToModel(station)));
+    .then(result => result.map(station => stationToModel(station)))
+    .then(result => result.sort((a, b) => a.id - b.id))
 }
 
 function stationToModel(data) {
