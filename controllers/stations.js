b27ff7c01079f1a74ab7b29b6a77d4737591a44f
olo2552 <a.wielgorski@protonmail.com>
2017-09-24 15:48:25 +0200

    added routing, measurements working

---
+++ controllers/stations.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+function fetchStations() {
+  return fetch('http://pomiary.gdmel.pl/rest/stations')
+    .then(res => res.json())
+    .then(json => json.data)
+    .then(result => result.map(station => stationToModel(station)));
+}
+
+function stationToModel(data) {
+  return {
+    id: data.no - 1,
+    externalId: data.no,
+    name: data.name,
+    active: data.active,
+    sensors: {
+      rain: data.rain,
+      water: data.water,
+      windDir: data.winddir,
+      windLevel: data.windlevel
+    }
+  };
+}
+
+module.exports = fetchStations
---
+++ routes/stations.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const fetchStations = require('../controllers/stations')
+
+router.get('/', (req, res) => {
+	fetchStations()
+		.then(data => res.json(data))
+})
+
+router.get('/:id', (req, res) => {
+	fetchStations()
+		.then(data => data.find(station => station.id === Number(req.params.id)))
+		.then(data => res.json(data))
+})
+
+
+module.exports = router
