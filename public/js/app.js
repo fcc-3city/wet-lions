6bca4cf40e9c24d10316d7ac7d31cd3f0107a699
Lukasz Szmelc <lszmelc@gmail.com>
2017-10-17 18:41:28 +0200

    Add date-picker to the chart

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
 
-  // Populate list with station names
+  // Populate sidebar with station names
   $.getJSON(window.location + 'api/stations', function(json) {
   	for(station of json) {
   		$('#slide-out').append('<li><a href="#!">' + station.name + '</a></li>');
@@ -16,46 +16,67 @@ $(document).ready(function() {
 
 
   // CHARTS
+  $('#date-picker').change(function() {
+  	let date = this.value.split("-").join('')
+  	console.log(date);
 
-		var ctx = document.getElementById("myChart").getContext('2d');
-		var myChart = new Chart(ctx, {
-		    type: 'bar',
-		    data: {
-		        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-		        datasets: [{
-		            label: '# of Votes',
-		            data: [12, 19, 3, 5, 2, 3],
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.2)',
-		                'rgba(54, 162, 235, 0.2)',
-		                'rgba(255, 206, 86, 0.2)',
-		                'rgba(75, 192, 192, 0.2)',
-		                'rgba(153, 102, 255, 0.2)',
-		                'rgba(255, 159, 64, 0.2)'
-		            ],
-		            borderColor: [
-		                'rgba(255,99,132,1)',
-		                'rgba(54, 162, 235, 1)',
-		                'rgba(255, 206, 86, 1)',
-		                'rgba(75, 192, 192, 1)',
-		                'rgba(153, 102, 255, 1)',
-		                'rgba(255, 159, 64, 1)'
-		            ],
-		            borderWidth: 1
-		        }]
-		    },
-		    options: {
-		        scales: {
-		            yAxes: [{
-		                ticks: {
-		                    beginAtZero:true
-		                }
-		            }]
-		        }
-		    }
-		});	  
+	  $.getJSON(window.location + 'api/measurments/0/' + date, function(json) {
+	  	
 
-  
+	  	function parseDate() {
+	  		const labels = [];
+	  		for(entry of json) {
+	  			let date = new Date(entry.date);
+	  			const leadingZero = (num) => `0${num}`.slice(-2);
+				  const labelDate = [date.getHours(), date.getMinutes()]
+				  .map(leadingZero)
+				  .join(':');
+				  console.log(labelDate);
+				  labels.push(labelDate);
+	  		}
+	  		return labels;
+	  	} // funkcja parsująca datę do używalnego formatu i dająca labele do wykresu
+			
+			function getWaterLevel() {
+				const waterLevels = [];
+				for(entry of json) {
+					waterLevels.push(entry.water)
+				}
+				return waterLevels;
+			}
+
+
+			var ctx = document.getElementById("myChart").getContext('2d');
+			var myChart = new Chart(ctx, {
+			    type: 'line',
+			    data: {
+			        labels: parseDate(),
+			        datasets: [{
+			            label: 'mm',
+			            data: getWaterLevel(),
+			            backgroundColor:'rgba(255, 99, 132, 0.2)',		            
+			            borderWidth: 1
+			        }]
+			    },
+			    options: {
+			        scales: {
+			            yAxes: [{
+			                ticks: {
+			                    beginAtZero:true
+			                }
+			            }]
+			        }
+			    }
+			});	 
+
+	  });
+  })
+
+
+
+ 
+
+
   // CHARTS END
 
 }); 
