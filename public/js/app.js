bc60a364e69550dfd6923b0f5b48be18cad5baa1
Norbert Szulc <norbert@not7cd.net>
2017-12-25 16:14:12 +0100

    Major tweaks

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-// MATERIALIZE JQUERY
+    // MATERIALIZE JQUERY
 
   $('.button-collapse').sideNav()
   $('select').material_select()
@@ -13,17 +13,31 @@ $(document).ready(function () {
     belowOrigin: false, // Displays dropdown below the button
     alignment: 'left', // Displays dropdown with edge aligned to the left of button
     stopPropagation: false // Stops event propagation
-  }
-  )
+  })
 
-// END MATERIALIZE JQUERY
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 5, // Creates a dropdown of 15 years to control year,
+        // format: 'yyyy-mm-dd',
+    formatSubmit: 'yyyy-mm-dd',
+    hiddenPrefix: 'date',
+        // hiddenName: true,
+    today: 'Dziś',
+    clear: 'Wyczyść',
+    close: 'Ok',
+    closeOnSelect: true
+  })
+
+  $('.modal').modal()
+
+    // END MATERIALIZE JQUERY
 
   function populateSideNav () {
-  // Populate select tag with station names and define whether they're active
+        // Populate select tag with station names and define whether they're active
     $.getJSON('/api/stations', json => {
       for (station of json) {
         $('#station-form')
-          .append(`<li>
+                    .append(`<li>
             <input id=${station.id}_station value=${station.id} name='station' type='radio'/>
             <label for=${station.id}_station>${station.active == true ? '🌧' : '🌤'} ${station.name}</label>
             </li>`)
@@ -32,109 +46,112 @@ $(document).ready(function () {
   }
   populateSideNav()
 
-  // CHARTS
+    // CHARTS
   $('.select-station').change(function () {
-    const date = $('#date-picker').val().split('-').join('')
+    const date = $("input[name='date_submit']").val()
     const station = $("input[type='radio']:checked").val()
 
-    $.getJSON(`/api/measurments/${station}/${date}`, json => {
-      function parseDate () {
-        const labels = []
-        for (entry of json) {
-          let date = new Date(entry.date)
-          const leadingZero = (num) => `0${num}`.slice(-2)
-          const labelDate = [date.getHours(), date.getMinutes()]
-          .map(leadingZero)
-          .join(':')
-          console.log(labelDate)
-          labels.push(labelDate)
-        }
-        return labels
-      } // funkcja parsująca datę do używalnego formatu i dająca labele do wykresu
-
-      function getWaterLevel () {
-        const waterLevels = []
-        for (entry of json) {
-          if (entry.water == 4000) {
-            entry.water = 0
-            waterLevels.push(entry.water)
-          } else {
-            waterLevels.push(entry.water)
+    if (typeof date !== 'undefined' && typeof station !== 'undefined') {
+      console.log(`/api/measurments/${station}/${date}`)
+      $.getJSON(`/api/measurments/${station}/${date}`, json => {
+        function parseDate () {
+          const labels = []
+          for (entry of json) {
+            let date = new Date(entry.date)
+            const leadingZero = (num) => `0${num}`.slice(-2)
+            const labelDate = [date.getHours(), date.getMinutes()]
+                            .map(leadingZero)
+                            .join(':')
+            // console.log(labelDate)
+            labels.push(labelDate)
           }
-        }
-        return waterLevels
-      }
+          return labels
+        } // funkcja parsująca datę do używalnego formatu i dająca labele do wykresu
 
-      function getWindSpeed () {
-        const windSpeeds = []
-        for (entry of json) {
-          if (entry.windLevel == 4000) {
-            entry.windLevel = 0
-            windSpeeds.push(entry.windLevel)
-          } else {
-            windSpeeds.push(entry.windLevel)
+        function getWaterLevel () {
+          const waterLevels = []
+          for (entry of json) {
+            if (entry.water == 4000) {
+              entry.water = 0
+              waterLevels.push(entry.water)
+            } else {
+              waterLevels.push(entry.water)
+            }
           }
+          return waterLevels
         }
-        return windSpeeds
-      }
 
-      $.getJSON(`/api/stations/${station}`, station => {
-        document.getElementById('station-header').innerHTML = `Stacja ${station.name}`
-        document.title = `🌧 ${station.name}`
-      })
+        function getWindSpeed () {
+          const windSpeeds = []
+          for (entry of json) {
+            if (entry.windLevel == 4000) {
+              entry.windLevel = 0
+              windSpeeds.push(entry.windLevel)
+            } else {
+              windSpeeds.push(entry.windLevel)
+            }
+          }
+          return windSpeeds
+        }
 
-      // WATER LEVEL CHART
+        $.getJSON(`/api/stations/${station}`, station => {
+          document.getElementById('station-header').innerHTML = `📡 ${station.name}`
+          document.title = `🌧 ${station.name}`
+        })
 
-      var ctx = document.getElementById('water-level').getContext('2d')
-      var waterChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: parseDate(),
-          datasets: [{
-            label: 'mm',
-            data: getWaterLevel(),
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
+                // WATER LEVEL CHART
+
+        const ctx = document.getElementById('water-level').getContext('2d')
+        const waterChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: parseDate(),
+            datasets: [{
+              label: 'm',
+              data: getWaterLevel(),
+              backgroundColor: 'rgba(99, 99, 132, 0.5)',
+              borderWidth: 1
             }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
           }
-        }
-      })
-      // WATER LEVEL CHART END8
+        })
+                // WATER LEVEL CHART END8
 
-      // WIND SPEED CHART
-      var ctw = document.getElementById('wind-speed').getContext('2d')
-      var windChart = new Chart(ctw, {
-        type: 'line',
-        data: {
-          labels: parseDate(),
-          datasets: [{
-            label: 'm/s',
-            data: getWindSpeed(),
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
+                // WIND SPEED CHART
+        const ctw = document.getElementById('wind-speed').getContext('2d')
+        const windChart = new Chart(ctw, {
+          type: 'line',
+          data: {
+            labels: parseDate(),
+            datasets: [{
+              label: 'm/s',
+              data: getWindSpeed(),
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderWidth: 1
             }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
           }
-        }
-      })
-      // WIND SPEED CHART END
-    }) // end getJSON
+        })
+                // WIND SPEED CHART END
+      }) // end getJSON
+    }
   }) // end date-picker change
 
-  // CHARTS END
+    // CHARTS END
 })
